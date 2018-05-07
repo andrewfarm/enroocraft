8d373e504b5a1c09615655cf30482f04dbb91e25
andrewfarm <Andrew.Farm@tufts.edu>
2018-05-07 04:11:28 -0400

    Aesthetic changes

--- renderer.cpp
+++ renderer.cpp
@@ -265,7 +265,7 @@ void Renderer::render() {
     glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
     glViewport(0, 0, width, height);
     
-    glClearColor(0.6f, 0.8f, 1.0f, 1.0f);
+    glClearColor(0.5f, 0.8f, 1.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LEQUAL);
