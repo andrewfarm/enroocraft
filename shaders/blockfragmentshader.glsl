68d2d503e6262fae866c3a23a9ea5daf112fa1e3
andrewfarm <Andrew.Farm@tufts.edu>
2018-05-07 03:49:46 -0400

    move fog to block fragment shader instead of postprocessing

--- shaders/blockfragmentshader.glsl
+++ shaders/blockfragmentshader.glsl
@@ -7,9 +7,14 @@ uniform sampler2D u_Texture;
 
 layout(location = 0) out vec4 color;
 
+const vec3 fogColor = vec3(0.8, 0.9, 1.0);
+
 void main() {
     vec4 texColor = texture(u_Texture, v_TexUV);
     float ambientLight = 0.6;
     float directionalLight = clamp(dot(v_Normal, normalize(vec3(0.1f, 1.0f, 0.2f))), 0.0f, 1.0f);
-    color = vec4(texColor.rgb * clamp(ambientLight + directionalLight, 0.0f, 1.0f), texColor.a);
+    float fog = pow(gl_FragCoord.z, 250.0);
+    vec3 colorWithoutFog = texColor.rgb * clamp(ambientLight + directionalLight, 0.0f, 1.0f);
+    vec3 colorWithFog = (colorWithoutFog * (1.0f - fog)) + (fogColor * fog);
+    color = vec4(colorWithFog, texColor.a);
 }
--- shaders/screenfragmentshader.glsl
+++ shaders/screenfragmentshader.glsl
@@ -9,7 +9,7 @@ out vec3 color;
 
 void main() {
     float depth = texture(u_DepthTexture, v_UV).x;
-    float fog = depth == 1.0 ? 0.0 : pow(depth, 250.0);
-    vec3 fogColor = vec3(0.8, 0.9, 1.0);
-    color = texture(u_ColorTexture, v_UV).rgb * (1.0 - fog) + (fogColor * fog);
+//    float fog = depth == 1.0 ? 0.0 : pow(depth, 250.0);
+//    vec3 fogColor = vec3(0.8, 0.9, 1.0);
+    color = texture(u_ColorTexture, v_UV).rgb;
 }
